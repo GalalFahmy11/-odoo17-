efde32423203be07346b27a297325f39bf9e6cc5
ahmed ali <fathyhekal12@gmail.com>
2024-11-18 19:51:57 +0200

    Add files via upload

---
+++ projects/myadds/testapp/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "testapp",
+    'summary': "this is my first app",
+    'description': """ welcome to my first app """,
+    'author': "gamal",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base','mail'],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+        'views/Completed_Orders.xml',
+        'views/contacts_new.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ projects/myadds/testapp/models/contacts_new.py
@@ -0,0 +1,8 @@
+# from odoo import models, fields
+#
+# class ContactsNew(models.Model):
+#     _inherit = 'res.partner'
+#
+#     my_notes = fields.Text(string="Notes")
+#     my_sales_team_phone = fields.Char(string="Sales Team Phone")
+#     my_sales_team_address = fields.Char(string="Sales Team Address")
